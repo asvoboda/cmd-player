42495e7c2965d97693efeea033cc5c6b90dcde1e
Andrew Svoboda <svoboda.andrew@gmail.com>
2012-06-19 23:05:54 -0400

    no longer blocking call to play music, can now pick and choose other songs while waiting

--- player.py
+++ player.py
@@ -12,25 +12,31 @@ def mciSend(s):
 def playMP3(mp3Name):
     mciSend("Close All")
     mciSend("Open \"%s\" Type MPEGVideo Alias theMP3" % mp3Name)
-    mciSend("Play theMP3 Wait")
-    mciSend("Close theMP3")
+    #mciSend("Play theMP3 Wait")
+    mciSend("Play theMP3")
+   
+def closeMP3():
+    mciSend("Close All")
    
 def main():
     while True:
         listdir = os.listdir(os.getcwd())
-        print os.getcwd()
+        print "$ %s\n" % os.getcwd()
         for (i, name) in zip(range(len(listdir)), listdir):
-            print "[%s] %s" % (i, name)
+            print '[%s] %s' % (i, name)
         cmd = raw_input("--> ")
         opt = cmd.split(" ")
         try:
-            if int(opt[0]) in range(len(listdir)):
+            if (int(opt[0])) in range(len(listdir)):
                 f = listdir[int(opt[0])]
                 if os.path.isdir(f):
                     os.chdir(f)
                 elif os.path.isfile(f):
-                    print "Playing ... %s" % f
-                    playMP3(os.path.join(os.getcwd(), f))
+                    if f.endswith("mp3"):
+                        print "\nPlaying ... %s\n" % f
+                        playMP3(os.path.join(os.getcwd(), f))
+                    else:
+                        print "Not an .mp3 file"
                 else:
                     print "%s is not a valid thing" % f
         except ValueError, e:
@@ -38,6 +44,7 @@ def main():
             
         if opt[0] == "up":
             os.chdir("..")
+            
         elif opt[0] == "cd":
             newpath = " ".join(opt[1:])
             if "My " in newpath:
@@ -46,6 +53,11 @@ def main():
                 os.chdir(" ".join(opt[1:]))
             else:
                 print "%s is not a valid directory" % " ".join(opt[1:])
+        elif opt[0] == "stop":
+            closeMP3()
+            
+        elif opt[0] == "exit":
+            sys.exit(0)
             
 if __name__ == "__main__":
     main()
